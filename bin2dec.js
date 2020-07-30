474a6b5672b0f9edd0f6b911c96c6b99a4b9c3b5
Elias <eli.bueno9@gmail.com>
2020-07-30 15:52:37 -0300

    Emphasizing the final answer

--- bin2dec.js
+++ bin2dec.js
@@ -8,9 +8,14 @@ function getValue() {
             //If the user type something different from 0 and 1
             if(value[letter] !== "0" && value[letter] !== "1") 
                 throw `Error: you must type only 0 or 1. You typed '${value[letter]}' at your char number ${letter + 1}.`;
-            else 
+            else {
                 document.getElementById("answer").innerHTML = `The correspoding 
-                decimal number of ${value} is ${parseInt(value, 2)}`;
+                decimal number of ${value} is ` +
+                "<br />" +
+                "<span style='color: lightgreen; font-size: 40px'>" +
+                ` ${parseInt(value, 2)}` +
+                "</span>";
+            }
         }
     } catch (error) {
         document.getElementById("answer").innerHTML = error;
